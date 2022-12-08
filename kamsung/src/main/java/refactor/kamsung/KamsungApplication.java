d90d2364e400760cccaab95c35064707e80a0c78
WonSubSub <boxer1995@naver.com>
2022-12-08 15:22:31 +0900

    feat: 스프링부트 프로젝트 생성

---
+++ kamsung/src/main/java/refactor/kamsung/KamsungApplication.java
@@ -0,0 +1,13 @@
+package refactor.kamsung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamsungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamsungApplication.class, args);
+	}
+
+}
---
+++ kamsung/src/test/java/refactor/kamsung/KamsungApplicationTests.java
@@ -0,0 +1,13 @@
+package refactor.kamsung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamsungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
