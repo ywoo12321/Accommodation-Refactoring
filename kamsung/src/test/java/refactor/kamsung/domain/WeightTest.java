d1653eb0a0aa4d43644f79c995b95bf6f4768dc4
WonSubSub <boxer1995@naver.com>
2022-12-20 20:54:05 +0900

    feat: 가중치 엔티티에서 태그가 제대로 구해지는지 테스트

---
+++ kamsung/src/test/java/refactor/kamsung/domain/WeightTest.java
@@ -0,0 +1,36 @@
+package refactor.kamsung.domain;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.Assert.*;
+
+public class WeightTest {
+
+//    private Weight weight;
+
+    private Weight createWeight(int natural, int modern, int industrial, int asia) {
+        Weight weight = new Weight(natural, modern, industrial, asia);
+        return weight;
+    }
+
+    @Test
+    public void 숙소_태그() throws Exception {
+
+        //given
+        Weight natural = createWeight(100,12,13,14);
+        Weight modern = createWeight(1,100,13,14);
+        Weight industrial = createWeight(1,12,100,14);
+        Weight asia = createWeight(1,12,13,100);
+
+        //when
+
+
+        //then
+        Assertions.assertEquals(natural.getMain(), "natural");
+        Assertions.assertEquals(modern.getMain(), "modern");
+        Assertions.assertEquals(industrial.getMain(), "industrial");
+        Assertions.assertEquals(asia.getMain(), "asia");
+    }
+
+}
