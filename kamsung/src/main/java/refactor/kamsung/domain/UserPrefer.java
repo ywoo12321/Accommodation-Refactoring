245d416e4dca0957c3e224c64017dc26fb5bc44a
WonSubSub <boxer1995@naver.com>
2022-12-20 19:19:22 +0900

    feat: 유저의 총 가중치를 구하는 로직 추가

--- kamsung/src/main/java/refactor/kamsung/domain/UserPrefer.java
+++ kamsung/src/main/java/refactor/kamsung/domain/UserPrefer.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,6 +22,31 @@ public class UserPrefer {
     private User user;
 
     private Weight weight;
+
+    public void setUserPrefer(User user) {
+        List<Like> likes = user.getLikes();
+        int natural = 0;
+        int modern = 0;
+        int industrial = 0;
+        int asia = 0;
+
+        for (int i = 0; i < likes.size(); i++) {
+            natural += likes.get(i).getLodging().getWeight().getNatural();
+            modern += likes.get(i).getLodging().getWeight().getModern();
+            industrial += likes.get(i).getLodging().getWeight().getIndustrial();
+            asia += likes.get(i).getLodging().getWeight().getAsia();
+        }
+
+        Weight weight = new Weight(natural, modern, industrial, asia);
+        this.weight = weight;
+    }
+
+    public static UserPrefer createUserPrefer(User user) {
+        UserPrefer userPrefer = new UserPrefer();
+        userPrefer.setUser(user);
+        userPrefer.setUserPrefer(user);
+        return userPrefer;
+    }
 
     // 선호 지역, 월별 선호도는 후에 추가
 }
