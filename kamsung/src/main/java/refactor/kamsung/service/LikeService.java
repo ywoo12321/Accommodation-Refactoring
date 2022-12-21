7fb9c39bfa9a0f0e43a51831c97b3b23f9cee3da
WonSubSub <boxer1995@naver.com>
2022-12-22 00:06:02 +0900

    feat: 좋아요 클릭,취소시 회원성향 업데이트 기능 추가

--- kamsung/src/main/java/refactor/kamsung/service/LikeService.java
+++ kamsung/src/main/java/refactor/kamsung/service/LikeService.java
@@ -6,8 +6,10 @@ import org.springframework.transaction.annotation.Transactional;
 import refactor.kamsung.domain.Like;
 import refactor.kamsung.domain.Lodging;
 import refactor.kamsung.domain.User;
+import refactor.kamsung.domain.UserPrefer;
 import refactor.kamsung.repository.LikeRepository;
 import refactor.kamsung.repository.LodgingRepository;
+import refactor.kamsung.repository.UserPreferRepository;
 import refactor.kamsung.repository.UserRepository;
 
 @Service
@@ -19,6 +21,21 @@ public class LikeService {
     private final LodgingRepository lodgingRepository;
     private final LikeRepository likeRepository;
 
+    private final UserPreferRepository userPreferRepository;
+
+    private final UserPreferService userPreferService;
+
+    @Transactional
+    public void updateUserPrefer(User user) {
+        if (user.getUserPrefer() == null) {
+            userPreferService.UserPrefer(user.getId());
+        } else {
+            UserPrefer userPrefer = userPreferRepository.findOne(user.getUserPrefer().getId());
+            userPrefer.makeUserPrefer(user);
+            user.setUserPrefer(userPrefer);
+        }
+    }
+
     //좋아요
     @Transactional
     public Long like(Long userId, Long lodgingId) {
@@ -27,6 +44,7 @@ public class LikeService {
         Lodging lodging = lodgingRepository.findOne(lodgingId);
 
         Like like = Like.createLike(user, lodging);
+        updateUserPrefer(user);
 
         likeRepository.save(like);
 
@@ -45,6 +63,7 @@ public class LikeService {
         user.getLikes().remove(like);
         Lodging lodging = lodgingRepository.findOne(like.getLodging().getId());
         lodging.getLikes().remove(like);  // 로직 맞는지 테스트, like 엔티티에 넣을지
+        updateUserPrefer(user);
 
     }
 }
