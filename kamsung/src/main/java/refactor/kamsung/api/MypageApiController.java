ea40e2727c7d217335a7355c1c934b3afed644ed
WonSubSub <boxer1995@naver.com>
2023-02-07 17:49:53 +0900

    feat: 마이페이지 찜리스트 가져오는 api 추가

---
+++ kamsung/src/main/java/refactor/kamsung/api/MypageApiController.java
@@ -0,0 +1,51 @@
+package refactor.kamsung.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import refactor.kamsung.domain.Like;
+import refactor.kamsung.domain.Lodging;
+import refactor.kamsung.domain.User;
+import refactor.kamsung.service.LikeService;
+import refactor.kamsung.service.LodgingService;
+import refactor.kamsung.service.UserPreferService;
+import refactor.kamsung.service.UserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MypageApiController {
+
+    private final UserPreferService userPreferService;
+    private final LodgingService lodgingService;
+    private final LikeService likeService;
+    private final UserService userService;
+
+    @Data
+    @AllArgsConstructor
+    static class MypageLikeListResponse {
+        private String lodgingImg;
+        private String lodgingName;
+
+        public MypageLikeListResponse(Like like) {
+            lodgingImg = like.getLodging().getMainImg();
+            lodgingName = like.getLodging().getName();
+        }
+    }
+
+    @GetMapping("/api/mypage/{id}") // 찜리스트
+    public List<MypageLikeListResponse> likeList(@PathVariable("id") Long userId) {
+        User user = userService.findOne(userId);
+        List<Like> likes = user.getLikes();
+
+        List<MypageLikeListResponse> result = likes.stream()
+                .map(l -> new MypageLikeListResponse(l))
+                .collect(Collectors.toList());
+        return result;
+    }
+}
