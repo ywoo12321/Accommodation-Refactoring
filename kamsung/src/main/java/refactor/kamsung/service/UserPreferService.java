7bfd4a763875df5293aab185c14a0af399c2ae57
WonSubSub <boxer1995@naver.com>
2023-02-17 15:34:05 +0900

    feat: 본인과 유사한 성향을 가진 Top10 숙소 구하는 기능 추가

--- kamsung/src/main/java/refactor/kamsung/service/UserPreferService.java
+++ kamsung/src/main/java/refactor/kamsung/service/UserPreferService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import refactor.kamsung.domain.Like;
+import refactor.kamsung.domain.Lodging;
 import refactor.kamsung.domain.User;
 import refactor.kamsung.domain.UserPrefer;
 import refactor.kamsung.repository.UserPreferRepository;
@@ -21,8 +22,8 @@ import static java.util.stream.Collectors.toList;
 public class UserPreferService {
 
     private final UserPreferRepository userPreferRepository;
-
     private final UserRepository userRepository;
+    private final LodgingService lodgingService;
 
     @Transactional
     public Long userPrefer(Long userId) {
@@ -113,4 +114,78 @@ public class UserPreferService {
         return result;
     }
 
+    @Transactional
+    public List<Integer> findLodgingWeights(Lodging lodging) {  // 코사인 유사도 구하는데 사용
+
+        List<Integer> weightList = new ArrayList<>();
+
+        weightList.add(lodging.getWeight().getNaturals());
+        weightList.add(lodging.getWeight().getModern());
+        weightList.add(lodging.getWeight().getIndustrial());
+        weightList.add(lodging.getWeight().getAsia());
+        weightList.add(lodging.getId().intValue());
+
+        return weightList;
+    }
+
+    @Transactional
+    public List<List<Integer>> findAllLodgingWeights() { // 코사인 유사도 구하는데 사용
+        List<Lodging> lodgingList = lodgingService.findLodgings();
+
+        List<List<Integer>> result = lodgingList.stream()
+                .map(l -> findLodgingWeights(l))
+                .collect(toList());
+
+        return result;
+    }
+
+    @Transactional
+    public List<List<Double>> getLodgingCosineSimilarities(User user) {
+        List<List<Integer>> lodgingWeights = findAllLodgingWeights();
+        List<List<Double>> result = new ArrayList<>();
+        List<Integer> myWeight = findWeights(user.getId());
+        for (List<Integer> weight : lodgingWeights) {
+            List<Double> cosineAndUserId = new ArrayList<>();
+            Integer dotProduct = 0;
+            double otherNorm = 0;
+            double myNorm = 0;
+            for (int i=0; i<4; i++) {
+                dotProduct += weight.get(i) * myWeight.get(i);
+                otherNorm += weight.get(i) * weight.get(i);
+                myNorm += myWeight.get(i) * myWeight.get(i);
+            }
+            double under = Math.sqrt(otherNorm) * Math.sqrt(myNorm);
+            double cosineSimilarity = (double) dotProduct / under;
+            cosineAndUserId.add(cosineSimilarity);
+            cosineAndUserId.add((double) weight.get(4));
+            result.add((cosineAndUserId));
+        }
+        return result;
+    }
+
+    @Transactional
+    public List<Lodging> getTopTenLodging(User user) {
+        List<List<Double>> cosineSimilarities = getLodgingCosineSimilarities(user);
+        List<Double> cosineSimilarityList = new ArrayList<>();
+        List<Long> lodgingIdList = new ArrayList<>();
+        List<Lodging> result = new ArrayList<>();
+
+        for (List<Double> cosineSimilarityAndLodgingId : cosineSimilarities) {
+            cosineSimilarityList.add(cosineSimilarityAndLodgingId.get(0));
+            long l = cosineSimilarityAndLodgingId.get(1).longValue();
+            lodgingIdList.add(l);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            int maxIndex = 0;
+            for (int j = 1; j < cosineSimilarityList.size(); j++) {
+                if (cosineSimilarityList.get(j) > cosineSimilarityList.get(maxIndex)) {
+                    maxIndex = j;
+                }
+            }
+            cosineSimilarityList.remove(maxIndex);
+            result.add(lodgingService.findOne(lodgingIdList.remove(maxIndex)));
+        }
+        return result;
+    }
 }
