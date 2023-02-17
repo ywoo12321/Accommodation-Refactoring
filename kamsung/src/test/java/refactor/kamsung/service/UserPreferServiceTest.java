02ae6cec72766c6b34cc0a088e12b14987c453c0
WonSubSub <boxer1995@naver.com>
2023-02-17 15:33:27 +0900

    feat: 본인과 유사한 성향을 가진 Top10 숙소 구하는 기능 테스트

--- kamsung/src/test/java/refactor/kamsung/service/UserPreferServiceTest.java
+++ kamsung/src/test/java/refactor/kamsung/service/UserPreferServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import refactor.kamsung.domain.*;
 import refactor.kamsung.repository.LikeRepository;
+import refactor.kamsung.repository.LodgingRepository;
 import refactor.kamsung.repository.UserPreferRepository;
 
 import javax.persistence.EntityManager;
@@ -27,14 +28,22 @@ public class UserPreferServiceTest {
     UserPreferService userPreferService;
     @Autowired
     UserPreferRepository userPreferRepository;
+    @Autowired
+    LodgingRepository lodgingRepository;
 
     private User createUser(UserPrefer userPrefer) {
         User user = new User();
-//        UserPrefer userPrefer = userPreferRepository.findOne(userPreferId);
         userPrefer.setUser(user);
         user.setUserPrefer(userPrefer);
         em.persist(user);
         return user;
+    }
+
+    private Lodging createLodging(Weight weight) {
+        Lodging lodging = new Lodging();
+        lodging.setWeight(weight);
+        lodgingRepository.save(lodging);
+        return lodging;
     }
 
     private Weight createWeight(int natural, int modern, int industrial, int asia) {
@@ -45,13 +54,11 @@ public class UserPreferServiceTest {
     private UserPrefer createUserPrefer(Weight weight) {
         UserPrefer userPrefer = new UserPrefer();
         userPrefer.setWeight(weight);
-//        userPreferRepository.findOne(userPrefer.getId()).setWeight(weight);
-//        userPreferRepository.save(userPrefer);
         return userPrefer;
     }
 
     @Test
-    public void 코사인유사도_높은_Top3_유저() throws Exception { // 총 유저 7명
+    public void 코사인유사도_높은_Top3_유저() throws Exception { // 총 유저 8명
 
         //given
         Weight weightMain = createWeight(13,53,20,9);
@@ -85,5 +92,70 @@ public class UserPreferServiceTest {
         //then
         Assertions.assertTrue(top3User.contains(user7));
         System.out.println(top3User);
+    }
+
+    @Test
+    public void 코사인유사도_높은_Top10_숙소() throws Exception { // 총 숙소 20개
+
+        //given
+        Weight userWeight = createWeight(13,53,20,9);
+        UserPrefer userPrefer = createUserPrefer(userWeight);
+        User user = createUser(userPrefer);
+
+        Weight weight1 = createWeight(15,25,35,40);
+        Lodging lodging1 = createLodging(weight1);
+        Weight weight2 = createWeight(20,20,15,40);
+        Lodging lodging2 = createLodging(weight2);
+        Weight weight3 = createWeight(25,30,30,30);
+        Lodging lodging3 = createLodging(weight3);
+        Weight weight4 = createWeight(30,0,0,70);
+        Lodging lodging4 = createLodging(weight4);
+        Weight weight5 = createWeight(35,70,0,0);
+        Lodging lodging5 = createLodging(weight5);
+        Weight weight6 = createWeight(40,10,10,41);
+        Lodging lodging6 = createLodging(weight6);
+        Weight weight7 = createWeight(45,0,23,34);
+        Lodging lodging7 = createLodging(weight7);
+        Weight weight8 = createWeight(50,16,13,12);
+        Lodging lodging8 = createLodging(weight8);
+        Weight weight9 = createWeight(55,10,10,50);
+        Lodging lodging9 = createLodging(weight9);
+        Weight weight10 = createWeight(60,50,10,10);
+        Lodging lodging10 = createLodging(weight10);
+        Weight weight11 = createWeight(65,10,50,10);
+        Lodging lodging11 = createLodging(weight11);
+        Weight weight12 = createWeight(70,40,30,10);
+        Lodging lodging12 = createLodging(weight12);
+        Weight weight13 = createWeight(75,10,30,10);
+        Lodging lodging13 = createLodging(weight13);
+        Weight weight14 = createWeight(80,10,10,20);
+        Lodging lodging14 = createLodging(weight14);
+        Weight weight15 = createWeight(85,10,0,0);
+        Lodging lodging15 = createLodging(weight15);
+        Weight weight16 = createWeight(90,0,10,10);
+        Lodging lodging16 = createLodging(weight16);
+        Weight weight17 = createWeight(95,40,43,10);
+        Lodging lodging17 = createLodging(weight17);
+        Weight weight18 = createWeight(5,30,20,40);
+        Lodging lodging18 = createLodging(weight18);
+        Weight weight19 = createWeight(0,12,24,68);
+        Lodging lodging19 = createLodging(weight19);
+        Weight weight20 = createWeight(10,10,10,10);
+        Lodging lodging20 = createLodging(weight20);
+
+        //when
+        List<Lodging> top10Lodging = userPreferService.getTopTenLodging(user);
+
+        //then
+        Assertions.assertTrue(top10Lodging.contains(lodging5));
+        Assertions.assertTrue(top10Lodging.contains(lodging3));
+        Assertions.assertTrue(top10Lodging.contains(lodging20));
+        Assertions.assertTrue(top10Lodging.contains(lodging10));
+        Assertions.assertTrue(top10Lodging.contains(lodging18));
+        Assertions.assertTrue(top10Lodging.contains(lodging12));
+        Assertions.assertTrue(top10Lodging.contains(lodging1));
+        Assertions.assertTrue(top10Lodging.contains(lodging2));
+        Assertions.assertTrue(top10Lodging.contains(lodging17));
+        Assertions.assertTrue(top10Lodging.contains(lodging8));
     }
 }
