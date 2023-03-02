342f383deb14b41b8f48ed6f1b7b01cab9fc50ca
WonSubSub <boxer1995@naver.com>
2023-03-02 13:23:54 +0900

    feat: 지역별로 숙소 가져오는 기능 추가

--- kamsung/src/main/java/refactor/kamsung/repository/LodgingRepository.java
+++ kamsung/src/main/java/refactor/kamsung/repository/LodgingRepository.java
@@ -2,6 +2,7 @@ package refactor.kamsung.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import refactor.kamsung.domain.Address;
 import refactor.kamsung.domain.Lodging;
 import refactor.kamsung.domain.User;
 
@@ -31,6 +32,27 @@ public class LodgingRepository {
         return em.createQuery("select l from Lodging l where l.weight.main = :main",
                         Lodging.class)
                 .setParameter("main", main)
+                .getResultList();
+    }
+
+    public List<Lodging> findOneByName(String name) {
+        return em.createQuery("select l from Lodging l where l.name = :name",
+                Lodging.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+    public List<Lodging> findByFirstAddress(String firstAddress) {
+        return em.createQuery("select l from Lodging l where l.address.firstAddress = :firstAddress",
+                        Lodging.class)
+                .setParameter("firstAddress", firstAddress)
+                .getResultList();
+    }
+
+    public List<Lodging> findBySecondAddress(String secondAddress) {
+        return em.createQuery("select l from Lodging l where l.address.secondAddress = :secondAddress",
+                        Lodging.class)
+                .setParameter("secondAddress", secondAddress)
                 .getResultList();
     }
 }
--- kamsung/src/main/java/refactor/kamsung/service/LodgingService.java
+++ kamsung/src/main/java/refactor/kamsung/service/LodgingService.java
@@ -3,6 +3,7 @@ package refactor.kamsung.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import refactor.kamsung.domain.Address;
 import refactor.kamsung.domain.Lodging;
 import refactor.kamsung.repository.LodgingRepository;
 
@@ -25,5 +26,17 @@ public class LodgingService {
 
     public List<Lodging> findLodgingsByTag(String main) {
         return lodgingRepository.findByTag(main);
+    }
+
+    public List<Lodging> findOneByName(String lodgingName) {
+        return lodgingRepository.findOneByName(lodgingName);
+    }
+
+    public List<Lodging> findByFirstAddress(String firstAddress) {
+        return lodgingRepository.findByFirstAddress(firstAddress);
+    }
+
+    public List<Lodging> findBySecondAddress(String secondAddress) {
+        return lodgingRepository.findBySecondAddress(secondAddress);
     }
 }
